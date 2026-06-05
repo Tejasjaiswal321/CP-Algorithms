fae8f0d0e8a20e19da724cbc09752c64a5826142
tejas.jaiswal <tejasjaiswal321@gmail.com>
2026-06-05 12:20:34 +0530

    LRU Cache on Leetcode done

---
+++ Interview_Bit_Submissions/hashing/LRUCache.cpp
@@ -0,0 +1,136 @@
+/*
+    Problem: LRU Cache
+    Platform: LeetCode
+    Link: https://leetcode.com/problems/lru-cache/description/
+
+    Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+    Implement the LRUCache class:
+
+    LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+    int get(int key) Return the value of the key if the key exists, otherwise return -1.
+    void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+    The functions get and put must each run in O(1) average time complexity.
+
+    
+
+    Example 1:
+
+    Input
+    ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+    [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+    Output
+    [null, null, null, 1, null, -1, null, -1, 3, 4]
+
+    Explanation
+    LRUCache lRUCache = new LRUCache(2);
+    lRUCache.put(1, 1); // cache is {1=1}
+    lRUCache.put(2, 2); // cache is {1=1, 2=2}
+    lRUCache.get(1);    // return 1
+    lRUCache.put(3, 3); // LRU key was 2, evicts key 2, cache is {1=1, 3=3}
+    lRUCache.get(2);    // returns -1 (not found)
+    lRUCache.put(4, 4); // LRU key was 1, evicts key 1, cache is {4=4, 3=3}
+    lRUCache.get(1);    // return -1 (not found)
+    lRUCache.get(3);    // return 3
+    lRUCache.get(4);    // return 4
+ 
+
+    Constraints:
+
+    1 <= capacity <= 3000
+    0 <= key <= 104
+    0 <= value <= 105
+    At most 2 * 105 calls will be made to get and put.
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+//Common Helper functions
+void manageInputOutput(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+}
+
+template<class T>
+void print_vector(vector<T> arr,string name = ""){
+    cout<<"print_vector : "<<name<<": ";
+    for(auto &i:arr){
+        cout<< i <<" ";
+    }
+    cout<<endl;
+}
+template<class T>
+void print_vector(set<T>st){
+    cout<<"print_set : "<<": ";
+    for(auto &i:st){
+        cout<< i <<" ";
+    }
+     cout<<endl;
+}
+
+typedef pair<int,int> pii;
+
+class LRUCache {
+    list<pii> cacheList;
+    
+    unordered_map<int,list<pii>::iterator>map;
+    int capacity;
+
+public:
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
+    int get(int key) {
+        if(map.contains(key)){
+            auto pr = *(map[key]);
+            cacheList.erase(map[key]);
+            cacheList.push_front(pr);
+            map[key] = cacheList.begin();
+
+            return (*(map[key])).second;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(map.contains(key)){
+            cacheList.erase(map[key]);
+        }
+        cacheList.push_front({key,value});
+        map[key] = cacheList.begin();
+        if(cacheList.size()>capacity){
+            // printf("erase {%d %d}",cacheList.back().first,cacheList.back().second);
+            map.erase(cacheList.back().first);
+            cacheList.pop_back();
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
+
+int main(){
+    manageInputOutput();
+    auto cache = LRUCache(2);
+    cache.put(2,1);
+    cache.put(1,1);
+    cache.put(2,3);
+    cache.put(4,1);
+    printf("%d %d",cache.get(1),cache.get(2));
+
+}
+
+
+
+
+
+
+
